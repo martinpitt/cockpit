c03e107f5f020638c287d5885a21cf96d9bea7be
Martin Pitt <mpitt@redhat.com>
2020-09-04 09:32:35 +0200

    WIP sosreport: Use LongRunningProcess
    journal lags behind ProcessState.DONE

--- pkg/sosreport/index.js
+++ pkg/sosreport/index.js
@@ -20,19 +20,16 @@
 import cockpit from "cockpit";
 import $ from "jquery";
 import { superuser } from "superuser";
+import { LongRunningProcess, ProcessState } from 'long-running-process';
 
 import '../lib/patternfly/patternfly-cockpit.scss';
 
 const _ = cockpit.gettext;
 
 var sos_task;
+var sos_output;
 var sos_archive_url;
 var sos_archive_files;
-
-function sos_init() {
-    // Start right away
-    sos_create();
-}
 
 function sos_error(message, extra) {
     $("#sos-alert, #sos-progress, #sos-download").hide();
@@ -47,104 +44,95 @@ function sos_error(message, extra) {
 }
 
 function sos_create() {
+    sos_archive_url = null;
+    sos_archive_files = [];
+
     $("#sos-progress .progress-bar").css("width", "0%");
     $("#sos-download, #sos-error").hide();
     $("#sos-cancel").text(_("Cancel"));
 
-    sos_archive_url = null;
-    sos_archive_files = [];
+    // HACK: SELinux has a pointless and broken exec_t policy, work around it: https://bugzilla.redhat.com/show_bug.cgi?id=1858740
+    sos_task.run(["sh", "-c", "exec sosreport --batch"], { /* pty: true */ })
+            .catch(error => sos_error(error.toString()));
+}
 
-    var task = cockpit.spawn(["sosreport", "--batch"],
-                             { superuser: true, err: "out", pty: true });
-    sos_task = task;
-
+function sos_process_output(text) {
     // TODO - Use a real API instead of scraping stdout once such
     //        an API exists.
+    console.log("XXX sos_process_output", text);
+    let plugins_count;
+    const progress_regex = /Running ([0-9]+)\/([0-9]+):/; // Only for sos < 3.6
+    const finishing_regex = /Finishing plugins.*\[Running: (.*)\]/;
+    const starting_regex = /Starting ([0-9]+)\/([0-9]+).*\[Running: (.*)\]/;
 
-    var output = "";
-    var plugins_count = 0;
-    var progress_regex = /Running ([0-9]+)\/([0-9]+):/; // Only for sos < 3.6
-    var finishing_regex = /Finishing plugins.*\[Running: (.*)\]/;
-    var starting_regex = /Starting ([0-9]+)\/([0-9]+).*\[Running: (.*)\]/;
-    var archive_regex = /Your sosreport has been generated and saved in:\s+(\/[^\r\n]+)/;
+    let m
+    let p = 0;
 
-    task.stream(function (text) {
-        if (sos_task == task) {
-            var m, p;
-            p = 0;
+    sos_output += text;
+    const lines = sos_output.split("\n");
+    for (let i = lines.length - 1; i >= 0; i--) {
+        if ((m = starting_regex.exec(lines[i]))) {
+            plugins_count = parseInt(m[2], 10);
+            p = ((parseInt(m[1], 10) - m[3].split(" ").length) / plugins_count) * 100;
+            break;
+        } else if ((m = finishing_regex.exec(lines[i]))) {
+            if (!plugins_count)
+                p = 100;
+            else
+                p = ((plugins_count - m[1].split(" ").length) / plugins_count) * 100;
+            break;
+        } else if ((m = progress_regex.exec(lines[i]))) {
+            p = (parseInt(m[1], 10) / parseInt(m[2], 10)) * 100;
+            break;
+        }
+    }
+    $("#sos-alert, #sos-progress").show();
+    $("#sos-progress .progress-bar").css("width", p.toString() + "%");
+}
 
-            output += text;
-            var lines = output.split("\n");
-            for (var i = lines.length - 1; i >= 0; i--) {
-                if ((m = starting_regex.exec(lines[i]))) {
-                    plugins_count = parseInt(m[2], 10);
-                    p = ((parseInt(m[1], 10) - m[3].split(" ").length) / plugins_count) * 100;
-                    break;
-                } else if ((m = finishing_regex.exec(lines[i]))) {
-                    if (!plugins_count)
-                        p = 100;
-                    else
-                        p = ((plugins_count - m[1].split(" ").length) / plugins_count) * 100;
-                    break;
-                } else if ((m = progress_regex.exec(lines[i]))) {
-                    p = (parseInt(m[1], 10) / parseInt(m[2], 10)) * 100;
-                    break;
-                }
+function sos_done() {
+    console.log("XXX sos_done", sos_output);
+    const archive_regex = /Your sosreport has been generated and saved in:\s+(\/[^\r\n]+)/;
+    const m = archive_regex.exec(sos_output);
+    if (m) {
+        const archive = m[1];
+        const basename = archive.replace(/.*\//, "");
+
+        // When running sosreport in a container on the
+        // Atomics, the archive path needs to be adjusted.
+        //
+        if (archive.indexOf("/host") === 0)
+            archive = archive.substr(5);
+
+        sos_archive_files = [archive, archive + ".md5"];
+
+        const query = window.btoa(JSON.stringify({
+            payload: "fsread1",
+            binary: "raw",
+            path: archive,
+            superuser: true,
+            max_read_size: 150 * 1024 * 1024,
+            external: {
+                "content-disposition": 'attachment; filename="' + basename + '"',
+                "content-type": "application/x-xz, application/octet-stream"
             }
-            $("#sos-alert, #sos-progress").show();
-            $("#sos-progress .progress-bar").css("width", p.toString() + "%");
-        }
-    });
-    task.done(function () {
-        if (sos_task == task) {
-            var m = archive_regex.exec(output);
-            if (m) {
-                var archive = m[1];
-                var basename = archive.replace(/.*\//, "");
+        }));
+        const prefix = (new URL(cockpit.transport.uri("channel/" + cockpit.transport.csrf_token))).pathname;
+        sos_archive_url = prefix + '?' + query;
+        $("#sos-progress, #sos-error").hide();
+        $("#sos-alert, #sos-download").show();
+        $("#sos-cancel").text(_("Close"));
+    } else {
+        sos_error(_("No archive has been created."), sos_output);
+    }
 
-                // When running sosreport in a container on the
-                // Atomics, the archive path needs to be adjusted.
-                //
-                if (archive.indexOf("/host") === 0)
-                    archive = archive.substr(5);
-
-                sos_archive_files = [archive, archive + ".md5"];
-
-                var query = window.btoa(JSON.stringify({
-                    payload: "fsread1",
-                    binary: "raw",
-                    path: archive,
-                    superuser: true,
-                    max_read_size: 150 * 1024 * 1024,
-                    external: {
-                        "content-disposition": 'attachment; filename="' + basename + '"',
-                        "content-type": "application/x-xz, application/octet-stream"
-                    }
-                }));
-                var prefix = (new URL(cockpit.transport.uri("channel/" + cockpit.transport.csrf_token))).pathname;
-                sos_archive_url = prefix + '?' + query;
-                $("#sos-progress, #sos-error").hide();
-                $("#sos-alert, #sos-download").show();
-                $("#sos-cancel").text(_("Close"));
-            } else {
-                sos_error(_("No archive has been created."), output);
-            }
-            sos_task = null;
-        }
-    });
-    task.fail(function (error) {
-        if (sos_task == task) {
-            sos_error(error.toString(), output);
-            sos_task = null;
-        }
-    });
+    sos_output = "";
 }
 
 function sos_cancel() {
-    if (sos_task) {
-        sos_task.close("cancelled");
-        sos_task = null;
-    }
+    if (sos_task)
+        sos_task.terminate();
+
     if (sos_archive_files.length > 0) {
         cockpit.spawn(["rm"].concat(sos_archive_files), { superuser: true, err: "message" })
                 .fail(function (error) {
@@ -169,9 +157,56 @@ function sos_download() {
     $('body').append(iframe);
 }
 
+// follow live output of the given unit, put into "output" <pre> area
+function showJournal(unitName, filter_arg) {
+    // run at most one instance of journal tailing
+    if (showJournal.journalctl)
+        return;
+
+    // reset previous output
+    output.innerHTML = "";
+
+    const argv = ["journalctl", "--output=cat", "--unit", unitName, "--follow", "--lines=all", filter_arg];
+    showJournal.journalctl = cockpit.spawn(argv, { superuser: "require", err: "message" })
+            .stream(data => output.append(document.createTextNode(data)))
+            .catch(ex => { output.innerHTML = JSON.stringify(ex) });
+}
+
+function sos_task_update(process) {
+    console.log("XXX sos_task_update", process.state);
+    switch (process.state) {
+        case ProcessState.INIT:
+            break;
+
+        case ProcessState.STOPPED:
+            sos_done();
+            break;
+
+        case ProcessState.RUNNING:
+            sos_output = "";
+
+            // follow live output
+            if (!sos_task_update.journalctl) {
+                // StateChangeTimestamp property is in µs since epoch, but journalctl expects seconds
+                const argv = ["journalctl", "--output=cat", "--unit", process.serviceName, "--follow", "--lines=all", "--since=@" + Math.floor(process.startTimestamp / 1000000)];
+                sos_task_update.journalctl = cockpit.spawn(argv, { superuser: "require", err: "message" })
+                        .stream(sos_process_output)
+                        .catch(error => sos_error(error.toString()));
+            }
+            break;
+
+        case ProcessState.FAILED:
+            sos_error(sos_output);
+            break;
+
+        default:
+            throw new Error("unexpected process.state: " + process.state);
+    }
+}
+
 function init() {
     $(function () {
-        $("#sos").on("show.bs.modal", sos_init);
+        $("#sos").on("show.bs.modal", sos_create);
         $("#sos-cancel").on("click", sos_cancel);
         $('#sos-download button').on('click', sos_download);
 
@@ -185,6 +220,8 @@ function init() {
 
         $(superuser).on("changed", update_admin_allowed);
         update_admin_allowed();
+
+        sos_task = new LongRunningProcess("cockpit-sosreport.service", sos_task_update);
 
         // Send a 'init' message.  This tells the tests that we
         // are ready to go.
